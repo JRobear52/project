383dbf64921eb252fafdfbe8073eb9c49d3455ce
Robert Jackson <r.h.jackson@mac.com>
2012-11-29 14:23:06 -0500

    Added purpose of program and handled divide by zero.

---
+++ mean.py
@@ -0,0 +1,12 @@
+# Prints the mean of the numbers given as arguments
+import sys
+
+if len(sys.argv) == 1:
+	print 'Error: No arguments given.'
+	sys.exit()
+
+sum = 0
+for num in sys.argv[1:]:
+    sum += float(num)
+
+print sum / (len(sys.argv) - 1)
